ab1bc4dea1a7ae284f04be0dab60663db1003e95
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-01-06 13:10:02 +0530

    Improvement 02

--- Express.js/01.Getting Started/index.js
+++ Express.js/01.Getting Started/index.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const app = express();
 
-console.dir(app);
+// console.dir(app);
+
+let port = 3000; // 8080
+
+app.listen(port, () => {
+    console.log(`app was listning on port: ${port}`);
+});
