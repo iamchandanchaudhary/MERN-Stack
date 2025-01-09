42a089ebbc00f05cd32edc5f0140ca712bf9251f
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-01-09 12:22:20 +0530

    Improvement 05

---
+++ Express.js/01.Getting Started/script.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+
+// console.dir(app);
+
+let port = 8080; // 8080
+
+app.listen(port, () => {
+    console.log(`app was listning on port: ${port}`);
+});
+
+let htmlCode = "<h1>Fruits List</h1> <ul> <li>Mango</li> <li>Apple</li></ul>"
+
+// Sending request
+app.use((req, res) => {
+    // console.log(req);
+    console.log("Request recived");
+
+    // res.send("This is a basic response!");
+    // res.send( {
+    //     name: "Chandan Chaudhary",
+    //     age: 18,
+    //     course: "BCA"
+    // })
+
+    res.send(htmlCode);
+})
