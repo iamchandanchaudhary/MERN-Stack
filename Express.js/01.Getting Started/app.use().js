16bcc8ed61f648517c9a694496771195c2087e52
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-01-09 12:26:37 +0530

    Improvement 05

--- Express.js/01.Getting Started/app.use().js
+++ Express.js/01.Getting Started/app.use().js
@@ -4,7 +4,7 @@ const app = express();
 
 // console.dir(app);
 
-let port = 8080; // 8080
+let port = 3001; // 8080
 
 app.listen(port, () => {
     console.log(`app was listning on port: ${port}`);
@@ -12,17 +12,19 @@ app.listen(port, () => {
 
 let htmlCode = "<h1>Fruits List</h1> <ul> <li>Mango</li> <li>Apple</li></ul>"
 
-// Sending request
+// => app.use() is use to Send request & response 
 app.use((req, res) => {
+    // => Sending request
     // console.log(req);
     console.log("Request recived");
 
+    // => Sending Response
     // res.send("This is a basic response!");
-    // res.send( {
-    //     name: "Chandan Chaudhary",
-    //     age: 18,
-    //     course: "BCA"
-    // })
+    res.send( {
+        name: "Chandan Chaudhary",
+        age: 18,
+        course: "BCA"
+    })
 
-    res.send(htmlCode);
+    // res.send(htmlCode);
 })
