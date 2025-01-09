87feb2a51a890077ff7c44efd6507018b32d63e1
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-01-09 11:14:23 +0530

    Improvement 05

--- Express.js/01.Getting Started/index.js
+++ Express.js/01.Getting Started/index.js
@@ -10,15 +10,19 @@ app.listen(port, () => {
     console.log(`app was listning on port: ${port}`);
 });
 
+let htmlCode = "<h1>This is Heading</h1>";
+
 // Sending request
 app.use((req, res) => {
     // console.log(req);
     console.log("Request recived");
 
-    // res.send("This is a basic response!");
+    res.send("This is a basic response!");
     res.send( {
         name: "Chandan Chaudhary",
         age: 18,
         course: "BCA"
     })
+
+    // res.send(htmlCode);
 })
