aef90448b4e6e2c81249273c1da530b7a3cf9c48
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-01-11 17:53:35 +0530

    Improvement 06

--- Express.js/02.EJS/index.js
+++ Express.js/02.EJS/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
-
 const app  = express();
+const path = require("path");
 
 const port = 3005;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.listen(port, () => {
     console.log(`App was listen on port: ${port}`);
 })
-
-app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     // res.send("This is home page.");
