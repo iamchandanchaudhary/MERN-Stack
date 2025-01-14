1260e78099fbcc9148695ee0a96fe33b2ce83b5c
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-01-14 09:18:58 +0530

    Upload 12 (Express)

---
+++ Express.js/03.Miscellaneous/Backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const port = 3012;
+
+app.listen(port, () => {
+    console.log(`App was listen on Port: ${port}`);
+})
+
+app.get("/", (req, res) => {
+    res.send("Home Page");
+})
+
+app.get("/register", (req, res) => {
+    let { username, passward } = req.query;
+    res.send(`<h3> This is a GET response. <br>Hello & Welcome ${ username }!</h3>`);
+})
+
+app.post("/register", (req, res) => {
+    // let { username, passward } = req.params;
+    res.send("<h1> This is a GET response. </h1>");
+})
