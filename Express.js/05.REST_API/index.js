40e3256138468a4bf22fcfde1bdddb4ea1d4d2a3
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-01-18 08:18:25 +0530

    Upload 16 (Express)

--- Express.js/05.REST_API/index.js
+++ Express.js/05.REST_API/index.js
@@ -9,7 +9,21 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [ {
+    username: "Chandan Chaudhary",
+    content: "Hello, from chandan_chaudhary.",
+    },
+    {
+    username: "Aman Agrawal",
+    content: "Hii, this is aman_agrawal.",
+    },
+    {
+    username: "Vishal Verma",
+    content: "Hello, my name is vishal.",
+    }, 
+];
 
 app.listen(port, () => {
     console.log(`App was listen on Port: ${port}.`);
@@ -17,4 +31,20 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
     res.send("Home Page.");
+})
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts });
+})
+
+app.get("/posts/new", (req, res) => {
+    res.render("newForm.ejs", { posts });
+})
+
+app.post("/posts", (req, res) => {
+    let { username, content } = (req.body);
+    posts.push({ username, content });
+    // res.send("Post wirking properly.");
+
+    res.redirect("/posts");
 })
