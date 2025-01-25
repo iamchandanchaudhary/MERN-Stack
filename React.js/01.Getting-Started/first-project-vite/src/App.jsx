1a43a76acce6178fc4cc4ceb3e62365bf6ee1915
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-01-25 17:34:25 +0530

    Upload 02 (ReactJS)

---
+++ React.js/01.Getting-Started/first-project-vite/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function Information() {
+  return (
+  <div>
+      <h1>Chandan Chaudhary</h1>
+      <p>Hello Everyone...</p>
+      <button>Click me!</button>
+  </div>
+  )
+}
+
+function App() {
+
+  return ( 
+    <div>
+      <Information/>
+    </div>
+  )
+}
+
+export default App
