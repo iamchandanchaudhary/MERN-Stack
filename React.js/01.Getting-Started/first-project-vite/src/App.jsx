880f76df4b67c2fe1da03488f381a0e36f2c8d76
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-01-25 17:52:40 +0530

    Improvement 17

--- React.js/01.Getting-Started/first-project-vite/src/App.jsx
+++ React.js/01.Getting-Started/first-project-vite/src/App.jsx
@@ -7,16 +7,24 @@ function Information() {
       <p>Hello Everyone...</p>
       <button>Click me!</button>
   </div>
-  )
+  );
+}
+
+function Title() {
+  return (
+    <p>This is another line.</p>
+  );
 }
 
 function App() {
 
   return ( 
     <div>
+      <h1>This is my Custom Component.</h1>
       <Information/>
+      <Title/>
     </div>
-  )
+  );
 }
 
 export default App
