ddef7013db3795b5ba68d81179160f8bdce71d47
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-01-26 10:13:16 +0530

    Improvement 20

--- React.js/01.Getting-Started/first-project-vite/src/components/Product.jsx
+++ React.js/01.Getting-Started/first-project-vite/src/components/Product.jsx
@@ -1,9 +1,11 @@
+import "./Product.css"
+
 function Product() {
     return (
-        <>
+        <div className="Product">
         <h2>This is Product Heading.</h2>
         <h4>This is Product Description.</h4>
-        </>
+        </div>
     );
 }
 
