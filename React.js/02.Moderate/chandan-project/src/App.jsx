9bd5470b5887f960b607f6f28055529f45b05432
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-01-27 08:40:04 +0530

    Improvement 21

--- React.js/02.Moderate/chandan-project/src/App.jsx
+++ React.js/02.Moderate/chandan-project/src/App.jsx
@@ -3,13 +3,13 @@ import Product from './component/Product';
 
 function App() {
   let fruit2 = "Mango";
-  // let amount3 = [22, 12, 43, 22, 32];
+  let colors = ["Green, ", "Yellow, ", "Red"];
   // console.log(amount3);
 
   return ( // Using Probs
     <> 
       <Product title="Apple" desc="A red sweet fruit" amount={15} />
-      <Product title={fruit2} desc="A yellow tasty fruit" amount={25} />
+      <Product title={fruit2} desc="A yellow tasty fruit" amount={25} colors={colors} />
       <Product title="Grapes" desc="A green sweet fruit" amount={20} />
     </>
   )
--- React.js/02.Moderate/chandan-project/src/component/Product.jsx
+++ React.js/02.Moderate/chandan-project/src/component/Product.jsx
@@ -1,10 +1,11 @@
-function Product( {title, desc, amount} ) {
+function Product( {title, desc, amount, colors} ) {
     return ( // Using Probs
-        <>
+        <div className="component-box">
             <h2>Title: {title}</h2>
             <h4>Description: {desc}.</h4>
             <h4>Amount: {amount}.</h4>
-        </>
+            <h4>{colors}</h4>
+        </div>
     );
 }
 
