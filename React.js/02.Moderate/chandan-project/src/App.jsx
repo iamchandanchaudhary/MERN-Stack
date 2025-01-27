2e80cdf18242d1000bcddf341f05bbcfb058cf5c
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-01-27 18:11:28 +0530

    Improvement 21

--- React.js/02.Moderate/chandan-project/src/App.jsx
+++ React.js/02.Moderate/chandan-project/src/App.jsx
@@ -4,13 +4,15 @@ import Product from './component/Product';
 function App() {
   let fruit2 = "Mango";
   let clr = ["Green, ", "Yellow, ", "Red"];
-  let feature = {a:"circle", b:"small", c:"multiple"};
+  let appleFtr = {a:"circlular", b:"big", c:"singular"};
+  let mangoFtr = {a:"modrate", b:"small", c:"multiple"};
+  let grapesFtr = {a:"circlular", b:"small", c:"multiple"};
 
   return ( // Using Probs
     <> 
-      <Product title="Apple" desc="A red sweet fruit" amount={15} />
-      <Product title={fruit2} desc="A yellow tasty fruit" amount={25} colors={clr} />
-      <Product title="Grapes" desc="A green sweet fruit" amount={20} feature={feature} />
+      <Product title="Apple" desc="A red sweet fruit" amount={15} feature={appleFtr} />
+      <Product title={fruit2} desc="A yellow tasty fruit" amount={25} colors={clr} feature={mangoFtr} />
+      <Product title="Grapes" desc="A green sweet fruit" amount={20} feature={grapesFtr} />
     </>
   )
 }
--- React.js/02.Moderate/chandan-project/src/component/Product.jsx
+++ React.js/02.Moderate/chandan-project/src/component/Product.jsx
@@ -1,11 +1,13 @@
 function Product( {title, desc, amount, colors, feature } ) {
+    // let list = colors.map((color) => <li>{color}</li>);
+
     return ( // Using Probs
         <div className="component-box">
             <h2>Title: {title}</h2>
             <p>Description: {desc}.</p>
             <h4>Amount: {amount}.</h4>
             <p>{colors}</p>
-            {/* <p>{feature.a}</p> */}
+            <p>{feature.a}</p>
         </div>
     );
 }
