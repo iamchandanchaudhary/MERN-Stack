a05a2555685efda1715abf966f26b1904f7b6008
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-01-27 09:10:40 +0530

    Improvement 21

--- React.js/02.Moderate/chandan-project/src/App.jsx
+++ React.js/02.Moderate/chandan-project/src/App.jsx
@@ -3,14 +3,14 @@ import Product from './component/Product';
 
 function App() {
   let fruit2 = "Mango";
-  let colors = ["Green, ", "Yellow, ", "Red"];
-  // console.log(amount3);
+  let clr = ["Green, ", "Yellow, ", "Red"];
+  let feature = {a:"circle", b:"small", c:"multiple"};
 
   return ( // Using Probs
     <> 
       <Product title="Apple" desc="A red sweet fruit" amount={15} />
-      <Product title={fruit2} desc="A yellow tasty fruit" amount={25} colors={colors} />
-      <Product title="Grapes" desc="A green sweet fruit" amount={20} />
+      <Product title={fruit2} desc="A yellow tasty fruit" amount={25} colors={clr} />
+      <Product title="Grapes" desc="A green sweet fruit" amount={20} feature={feature} />
     </>
   )
 }
--- React.js/02.Moderate/chandan-project/src/component/Product.jsx
+++ React.js/02.Moderate/chandan-project/src/component/Product.jsx
@@ -1,10 +1,11 @@
-function Product( {title, desc, amount, colors} ) {
+function Product( {title, desc, amount, colors, feature } ) {
     return ( // Using Probs
         <div className="component-box">
             <h2>Title: {title}</h2>
-            <h4>Description: {desc}.</h4>
+            <p>Description: {desc}.</p>
             <h4>Amount: {amount}.</h4>
-            <h4>{colors}</h4>
+            <p>{colors}</p>
+            {/* <p>{feature.a}</p> */}
         </div>
     );
 }
