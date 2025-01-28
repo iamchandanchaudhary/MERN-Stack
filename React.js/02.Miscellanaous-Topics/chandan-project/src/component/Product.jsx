a63b8d251e351c09a77e54c9224b0aa2c8c991b9
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-01-28 19:26:10 +0530

    Improvement 22

--- React.js/02.Miscellanaous-Topics/chandan-project/src/component/Product.jsx
+++ React.js/02.Miscellanaous-Topics/chandan-project/src/component/Product.jsx
@@ -2,10 +2,11 @@ function Product( {title, desc, amount, colors, feature } ) {
     // let list = colors.map((color) => <li>{color}</li>);
 
     // ==> Applying conditions
-
     // let isDiscount = amount > 20 ? "Discount of 5% (more then 20)." : "";
+
+    let styles = {backgroundColor : "lightyellow"};
     return ( // ==> Using Probs
-        <div className="component-box">
+        <div className="component-box" style={styles}>
             <h2>Title: {title}</h2>
             <p>Description: {desc}.</p>
             <h4>Amount: {amount}.</h4>
