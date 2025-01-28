ed8af131602bbc8e4d22f501f77bf7a9d8cf84f3
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-01-28 19:19:19 +0530

    Improvement 22

--- React.js/02.Miscellanaous Topics/chandan-project/src/component/Product.jsx
+++ React.js/02.Miscellanaous Topics/chandan-project/src/component/Product.jsx
@@ -17,25 +17,12 @@ function Product( {title, desc, amount, colors, feature } ) {
 
     // if(amount > 20) {
     //     return (
-    //         <div className="component-box">
-    //             <h2>Title: {title}</h2>
-    //             <p>Description: {desc}.</p>
-    //             <h4>Amount: {amount}.</h4>
-    //             <p>Discount of 5% (more then 20).</p>
-    //             <p>{colors}</p>
-    //             <p>{feature.a}</p>
-    //         </div>
+    //         
     //     );
     // } 
     // else {
     //     return (
-    //         <div className="component-box">
-    //             <h2>Title: {title}</h2>
-    //             <p>Description: {desc}.</p>
-    //             <h4>Amount: {amount}.</h4>
-    //             <p>{colors}</p>
-    //             <p>{feature.a}</p>
-    //         </div>
+    //         
     //     ); 
     // }
 }
