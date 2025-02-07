5741ec8f2043f1fd9913b264652bf7b2450862a9
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-02-07 13:03:17 +0530

    Improvement 34

--- React.js/Projects-using-React/todo-app/src/components/Todo-App.jsx
+++ React.js/Projects-using-React/todo-app/src/components/Todo-App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { v4 as uuid } from 'uuid';
 
 function TodoApp() {
-    let [todos, setTodos] = useState([ {task: "", id: uuid()} ]); // initial data
+    let [todos, setTodos] = useState([ {task: "Wake up", id: uuid()} ]); // initial data
     let [newTodo, setNewTodo] = useState("");  // new task
 
     function addTask() {
@@ -14,7 +14,7 @@ function TodoApp() {
         setNewTodo("");
     }
 
-    let updateTask = (event) => {
+    let updateNewTask = (event) => {
         // console.log(event.target.value);
         setNewTodo(event.target.value);
     }
@@ -25,34 +25,47 @@ function TodoApp() {
         
     }
 
+    let upperCaseAll = () => {
+        setTodos(( ) => 
+            todos.map((todo) => {
+                return {
+                    ...todos,
+                    task: todo.task.toUpperCase(),
+                }
+            })
+        )
+    }
+
     return (
         <div className="h-screen w-full flex flex-col items-center justify-start">
             <div className="w-[450px] p-4">
-            <h1 className="text-5xl font-bold my-4 text-center">Todo App</h1>
-            <div className="p-5 flex items-center justify-center">
-                <input type="text" placeholder="Enter Task" 
-                value={newTodo} 
-                onChange={updateTask} 
-                className="border-2 border-black py-1 px-3 rounded-lg" />
+                <h1 className="text-5xl font-bold my-4 text-center">Todo App</h1>
+                <div className="p-5 flex items-center justify-center">
+                    <input type="text" placeholder="Enter Task" 
+                    value={newTodo} 
+                    onChange={updateNewTask} 
+                    className="border-2 border-black py-1 px-3 rounded-lg" />
 
-                <button 
-                onClick={addTask} 
-                className="ml-5 rounded-lg bg-blue-500 hover:bg-blue-400 border-2 border-white focus:border-2 focus:border-blue-800 text-white px-3 py-1">Add Task</button>
+                    <button 
+                    onClick={addTask} 
+                    className="ml-5 rounded-lg bg-blue-500 hover:bg-blue-400 border-2 border-white focus:border-2 focus:border-blue-800 text-white px-3 py-1">Add Task</button>
 
-            </div>
+                </div>
 
-            <div>
-                <h1 className="text-xl font-bold flex">Your Tasks:- </h1>
-                <ul className="ml-3 mt-2">
-                    {todos.map((todo) => (
-                        <li key={todo.id}>
-                        <span> {todo.task} </span>    
-                        <button onClick={ () => { delTask(todo.id) }}>Delete</button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+                <div>
+                    <h1 className="text-xl font-bold flex">Your Tasks:- </h1>
+                    <ul className="ml-3 mt-2">
+                        {todos.map((todo) => (
+                            <li key={todo.id}>
+                            <span> {todo.task} </span>    
+                            <button className="text-red-600 text-sm ml-4"
+                            onClick={ () => { delTask(todo.id) }}>Delete</button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
 
+                <button onClick={upperCaseAll} className="ml-5 mt-5 rounded-lg bg-blue-500 hover:bg-blue-400 border-2 border-white focus:border-2 focus:border-blue-800 text-white px-3 py-1">Upper Case</button>
             </div>
         </div>
     );
