70be152d083fba7f1a3fef211befdfa21beb838e
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-02-08 19:09:29 +0530

    Improvement 37

--- React.js/Projects-using-React/LotteryTicket/src/components/LotteryTicket.jsx
+++ React.js/Projects-using-React/LotteryTicket/src/components/LotteryTicket.jsx
@@ -17,20 +17,23 @@ export default function LotteryTicket() {
     // setArrSum(sum(ticket));
 
     return (
-        <div>
-            <h1>Lottery Ticket</h1>
-            <div>
-                <span>{ticket[0]}</span>
-                <span>{ticket[1]}</span>
-                <span>{ticket[2]}</span>
+        <div className="w-full h-screen flex items-center justify-center flex-col">
+            <div className="bg-blue-400 text-white w-max px-12 py-4 flex flex-col items-center gap-4 rounded-lg shadow-lg">
+                <h1 className="text-4xl font-bold">Lottery Ticket</h1>
+                <div className="text-xl flex bg-white text-blue-400  border-black border-2 rounded transition-all duration-200 ease-in-out">
+                    <span className="border-e-2 border-black px-4 py-1">{ticket[0]}</span>
+                    <span className="border-e-2 border-black px-4 py-1">{ticket[1]}</span>
+                    <span className=" px-4 py-1">{ticket[2]}</span>
+                </div>
+
+                <p>Sum: {arrSum}</p>
+
+                <button className="border-[2px] border-gray-600 text-blue-400 bg-white rounded-md px-4 py-1"
+                onClick={generate}>Generate</button>
+
+                <p>{isWinning && "Congratulations, you won!"} {!isWinning && "You Lost!"}</p>
+
             </div>
-
-            <p>Sum: {arrSum}</p>
-
-            <button className="border-[1px] border-black text-white bg-blue-400 rounded-md px-2"
-            onClick={generate}>Generate</button>
-
-            <p>{isWinning && "Congratulations, you won!"}</p>
         </div>
     );
 }
