c422d3252cb3464c59ac68ec3174b8ced4b8f23d
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-02-15 14:31:25 +0530

    Improvement 38

--- React.js/Practice-react/my-react/src/components/Component.jsx
+++ React.js/Practice-react/my-react/src/components/Component.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 function Component() {
+
+    let [counter, setCounter] = useState(0);
+
+    let click = () => {
+        setCounter(counter + 1);
+    }
+
     return (
         <div>
-            <button className="text-white bg-pink-700 py-1 px-2 rounded mt-4 ml-4">Counter + </button>
+            <button onClick={click}
+            className="text-white bg-pink-600 py-1 px-2 rounded mt-4 ml-4">Counter + {counter}</button>
         </div>
     );
 }
