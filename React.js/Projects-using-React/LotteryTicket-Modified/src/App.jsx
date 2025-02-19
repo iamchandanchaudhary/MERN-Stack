0f12f6b55aada1ffdaa8178dd68ec0f9d5ac309b
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-02-19 09:01:33 +0530

    Improvement 41

--- React.js/Projects-using-React/LotteryTicket-Modified/src/App.jsx
+++ React.js/Projects-using-React/LotteryTicket-Modified/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css';
-// import LotteryTicket from './components/LotteryTicket';
 import TicketNum from './components/TicketNum';
 import Ticket from './components/Ticket';
 
@@ -10,6 +9,7 @@ function App() {
       <Ticket ticket={[1, 2, 3, 4, 5]} />
       <Ticket ticket={[1, 2, 3]} />
       <Ticket ticket={[1, 2, 3, 4, 5, 6, 7]} />
+      <Ticket ticket={[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]} />
     </div>
   )
 }
