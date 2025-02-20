1b162cdbde1b1ce3cd655514de7d797e08903410
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-02-20 08:17:53 +0530

    Improvement 42

--- React.js/Projects-using-React/LotteryTicket-Modified/src/App.jsx
+++ React.js/Projects-using-React/LotteryTicket-Modified/src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css';
 import TicketNum from './components/TicketNum';
 import Ticket from './components/Ticket';
+import LotteryTicket from './components/LotteryTicket';
 
 function App() {
 
   return (
     <div className='w-full h-screen flex items-center flex-col'>
-      <Ticket ticket={[1, 2, 3, 4, 5]} />
+
+      <LotteryTicket n={3} winningSum={15} />
+
+      {/* <Ticket ticket={[1, 2, 3, 4, 5]} />
       <Ticket ticket={[1, 2, 3]} />
       <Ticket ticket={[1, 2, 3, 4, 5, 6, 7]} />
-      <Ticket ticket={[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]} />
+      <Ticket ticket={[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]} /> */}
     </div>
   )
 }
--- React.js/Projects-using-React/LotteryTicket-Modified/src/components/LotteryTicket.jsx
+++ React.js/Projects-using-React/LotteryTicket-Modified/src/components/LotteryTicket.jsx
@@ -2,30 +2,30 @@ import { useState } from "react";
 import { genTicket, sum } from "./Generator.js";
 import Ticket from "./Ticket.jsx";
 
-export default function LotteryTicket(n, winningSum) {
+export default function LotteryTicket({n = 3, winningSum = 15}) {
 
     let [ticket, setTicket] = useState(genTicket(n));
 
-    let buyTicket = () => {
-        setTicket(genTicket(3));
-    } 
+    
 
     let isWinning = (sum(ticket) === winningSum);
-    let arrSum = sum(ticket);
+    // let arrSum = sum(ticket);
+
+    let buyTicket = () => {
+        setTicket(genTicket(n));
+    } 
 
     return (
         <div>
             <h1>Lottery Ticket</h1>
             <div>
-                <span>{ticket[0]}</span>
-                <span>{ticket[1]}</span>
-                <span>{ticket[2]}</span>
+                <Ticket ticket={ticket} />
             </div>
 
-            <p>Sum: {arrSum}</p>
+            {/* <p>Sum: {arrSum}</p> */}
 
             <button className="border-[1px] border-black text-white bg-blue-400 rounded-md px-2"
-            onClick={buyTicket}>Generate</button>
+            onClick={buyTicket}>Buy Ticket</button>
 
             <p>{isWinning && "Congratulations, you won!"}</p>
         </div>
