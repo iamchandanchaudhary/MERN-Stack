829d50e0c06ff040852a7e1a1e8510b7ccb6664e
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-02-22 09:00:51 +0530

    Improvement 46

--- React.js/Projects-using-React/Passward-Generator/src/components/Password-Generator.jsx
+++ React.js/Projects-using-React/Passward-Generator/src/components/Password-Generator.jsx
@@ -53,9 +53,9 @@ function PasswordGenerator() {
     }, [length, numberAllowed, smallLetterAllowed, capitalLetterAllowed, charAllowed, generator]);
     
     return (
-        <div className="h-screen w-full flex items-center justify-center flex-col text-black dark:text-white bg-pink-200 dark:bg-[#231557] dark:bg-gradient-to-l dark:from-[#231557] dark:to-[#140c30]">
+        <div className="h-screen w-full flex items-center justify-center flex-col text-black dark:text-white bg-gradient-to-l bg-[#02aab0] from-[#00cdac] to-[#02aab0] dark:bg-gradient-to-l dark:bg-[#040917] dark:from-[#00002b] dark:to-[#040917]">
 
-            <div className="w-max px-12 py-8 flex flex-col items-center bg-pink-100 dark:bg-[#231557] rounded-lg dark:drop-shadow-[0_0px_30px_rgba(59,130,246,0.6)]">
+            <div className="w-max px-12 py-8 flex flex-col items-center bg-[#fff] dark:bg-[#0a1338] rounded-lg drop-shadow-2xl groupdrop-shadow-[0_0px_30px_rgba(59,130,246,0.6)]">
                 <h1 className="text-5xl font-bold">Random Password Generator</h1>
                 <p className="my-4 text-base">Create strong and secure passwords to keep your account safe online.</p>
 
