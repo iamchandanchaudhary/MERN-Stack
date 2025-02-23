043af2a69e07115f847db4690c833e7ce5ad6c04
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-02-23 17:01:03 +0530

    Improvement 48

--- React.js/Projects-using-React/Passward-Generator/src/components/About.jsx
+++ React.js/Projects-using-React/Passward-Generator/src/components/About.jsx
@@ -1,23 +1,27 @@
+import "./About.css";
+
 function About() {
     return (
-        <section className="flex flex-col items-center w-max mt-12 bg-orange-300">
+        <section className="flex flex-col items-center w-max bg-orange-200 my-8 py-12 px-20 rounded-lg">
             <h1 className="text-4xl font-bold">What makes a password strong?</h1>
 
-            <div className="flex">
-                <div className="max-w-72">
-                    <i class="fa-solid fa-key"></i>
-                    <p>Long</p>
-                    <p>The longer a password, the more secure it is. A strong password should be at least 10 characters long.</p>
+            <div className="flex mt-12 gap-8">
+                <div className="max-w-72 bg-[#fff] px-8 py-4 flex items-center justify-center flex-col gap-4 rounded-lg">
+                    <i class="fa-solid fa-key text-4xl"></i>
+                    <p className="text-xl font-bold">Long</p>
+                    <p className="text-center">The longer a password, the more secure it is. A strong password should be at least 10 characters long.</p>
                 </div>
-                <div className="max-w-72">
-                    <i class="fa-solid fa-lock"></i>
-                    <p>Complex</p>
-                    <p>Strong passwords use a combination of letters, numbers, cases, and symbols to form an unpredictable string of characters that doesn't resemble words or names.</p>
+
+                <div className="max-w-72 bg-[#fff] px-8 py-4 flex items-center justify-center flex-col gap-4 rounded-lg">
+                    <i class="fa-solid fa-lock text-4xl"></i>
+                    <p className="text-xl font-bold">Complex</p>
+                    <p className="text-center">Strong passwords use a combination of letters, numbers, cases, and symbols to form an unpredictable string of characters that doesn't resemble words or names.</p>
                 </div>
-                <div className="max-w-72">
-                    <i class="fa-solid fa-shield-halved"></i>
-                    <p>Unique</p>
-                    <p>A strong password should be unique to each account to reduce vulnerability in the event of a hack.</p>
+
+                <div className="max-w-72 bg-[#fff] px-8 py-4 flex items-center justify-center flex-col gap-4 rounded-lg">
+                    <i class="fa-solid fa-shield-halved text-4xl"></i>
+                    <p className="text-xl font-bold">Unique</p>
+                    <p className="text-center">A strong password should be unique to each account to reduce vulnerability in the event of a hack.</p>
                 </div>
             </div>
         </section>
