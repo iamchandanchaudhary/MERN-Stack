5ffa1b44c2a2f98f7eef4ce79405906c55e0b51d
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-03-01 16:54:40 +0530

    Improvement 58

--- MongoDB/04.Mongoose/mongo/index.js
+++ MongoDB/04.Mongoose/mongo/index.js
@@ -1,3 +1,15 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/students');
+// mongoose.connect('mongodb://127.0.0.1:27017/students');
+
+main()
+    .then(() => {
+    console.log("Connection Successeful");
+    })
+    .catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/students');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
