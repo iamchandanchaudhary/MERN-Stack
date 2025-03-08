35d2f79066c755ab0ab1504f429b18bf26b0cc2c
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-03-08 21:26:10 +0530

    Improvement 67

--- React.js/Practice-react/react-practice/src/components/ChatMassage.jsx
+++ React.js/Practice-react/react-practice/src/components/ChatMassage.jsx
@@ -1,9 +1,12 @@
-function ChatMassage() {
+import ChatBotIcon from "./ChatBotIcon";
+
+function ChatMassage({ chat }) {
 
     return (
-        <div className="massage user-massage flex gap-3 flex-col items-end">
+        <div className={`massage ${chat.role === "model" ? 'bot' : 'user'}-massage flex gap-3 flex-col items-end`}>
+            {chat.role === "model" && <ChatBotIcon />}
             <p className="massage-text px-4 py-3 max-w-[75%] text-[15px] bg-[#6d4fc2] text-[#fff] rounded-s-xl rounded-tr-xl rounded-br-sm">
-                Lorem ipsum dolor sit amet.
+                {chat.text}
             </p>
         </div>
     );
