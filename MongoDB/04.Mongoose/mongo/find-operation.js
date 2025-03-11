1abed15b46b407ffc4ba3c3b1454a07fcdc4b48d
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-03-11 08:09:46 +0530

    Improvement 73

--- MongoDB/04.Mongoose/mongo/find-operation.js
+++ MongoDB/04.Mongoose/mongo/find-operation.js
@@ -26,13 +26,20 @@ const Student = mongoose.model("Student", studentSchema);
 // ])
 //     .then((data) => console.log(data));
 
-// => find all
-// Student.find({}).then((data) => console.log(data));
+// ==> find all
+Student.find({}).then((data) => console.log(data));
 
 
 // find using conditions
-// Student.find({age: {$gt: 20}}).then((data) => console.log(data));
+Student.find({age: {$gt: 20}}).then((data) => console.log(data));
 
-// Student.find({}).then((data) => console.log(data[0])); // getting single data at first index
-// Student.find({age: {$lt: 19}}).then((data) => console.log(data[0])); // getting single data at first index
-Student.find({}).then((data) => console.log(data)); // getting only names
+Student.find({}).then((data) => console.log(data[0])); // getting single data at first index
+Student.find({age: {$lt: 19}}).then((data) => console.log(data[0])); // getting single data at first index
+
+Student.find({age: {$lt: 19}}).then((data) => console.log("Student Name:", data[0].name)); // 
+
+// ==> find one
+Student.findOne({}).then((data) => console.log(data));
+Student.findOne({age: {$gte: 18}}).then((data) => console.log(data)); // with operation
+
+// ==> findById
