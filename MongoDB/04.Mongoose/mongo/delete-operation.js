cdc87c269781293e17f66e7c4c612da6ff8c23d4
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-03-12 17:29:23 +0530

    Upload 13 (MongoDB)

---
+++ MongoDB/04.Mongoose/mongo/delete-operation.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+main()
+    .then(() => console.log("Connection Successful."))
+    .catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/college4");
+}
+
+const employeeData = new mongoose.Schema({
+    name: String,
+    age: Number,
+    city: String,
+    sallary: Number
+});
+
+const Employee = mongoose.model("Employee", employeeData);
+
+// Employee.insertMany([
+//     {name: "ChanduBhaiii", age: 18, city: "Basti", sallary: 210000},
+//     {name: "Anand Kumar", age: 21, city: "Gandhi Nagar", sallary: 52100},
+//     {name: "Nidhi Pandey", age: 28, city: "Vanarash", sallary: 72100},
+//     {name: "Ajad Pathak", age: 23, city: "Kolkata", sallary: 37100},
+//     {name: "Akash Rajbhar", age: 26, city: "Vanarash", sallary: 62100},
+//     {name: "Alok Maurya", age: 20, city: "Kolkata", sallary: 67100}
+// ])
+//     .then((data) => console.log(data));
+
+Employee.find({}).then((data) => console.log(data));
+
+// ==> deleteOne
+Employee.deleteOne({age: 23}).then((res) => console.log(res));
+
+Employee.find({}).then((data) => console.log(data));
+
+// ==> deleteMany
+Employee.deleteMany({age: {$gte: 22}}).then((res) => console.log(res));
