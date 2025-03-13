2b5c965bbf8eb5dbb1154051b768ffe404809de2
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-03-13 12:06:50 +0530

    Upload 15 (MongoDB)

--- MongoDB/04.Mongoose/mongo/schema-validation.js
+++ MongoDB/04.Mongoose/mongo/schema-validation.js
@@ -8,7 +8,7 @@ async function main() {
     await mongoose.connect("mongodb://localhost:27017/book");
 }
 
-// Schema Validation
+// ==> Schema Validation
 const booksData = new mongoose.Schema({
     title: {
         type: String,
@@ -25,9 +25,11 @@ const booksData = new mongoose.Schema({
 const Book = mongoose.model("Book", booksData);
 
 Book.insertMany([
-    // {title: "Rich Dad & Poor Dad", author: "ChanduBhaiii", price: 240},
-    // {title: "Full Stack", author: "ChanduBhaiii"}
-        {author: "ChanduBhaiii", price: 240}
+    {title: "Rich Dad & Poor Dad", author: "Robert Kiyosaki", price: 240},
+    {title: "Full Stack", author: "ChanduBhaiii"},
+    // {author: "ChanduBhaiii", price: 240}, // => Invalide because it don't have title(required)
+    {title: "Physics XII", author: "HC Verma", price: "750"}, // => Valide case because it can be converted in Numbers(in Mongo)
+    // {title: "Mathematics XII", author: "RD Sharma", price: "abc"}, // => Invalide case because it can't be converted in Numbers(in Mongo)
 ])
 
 Book.find({}).then((data) => console.log(data));
