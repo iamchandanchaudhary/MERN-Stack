713aef75cf4613c7449337593acb18684f966db9
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-03-14 19:29:34 +0530

    Upload 16 (Mongo DB)

--- MongoDB/04.Mongoose/mongo/schema-validation-advance.js
+++ MongoDB/04.Mongoose/mongo/schema-validation-advance.js
@@ -12,21 +12,50 @@ const bookSchama = new mongoose.Schema({
     title: {
         type: String,
         required: true,
+        maxLength: 20,
     },
 
     author: {
         type: String,
     },
 
+    description: {
+        type: String,
+        lowercase: true,
+    },
+
     price: {
         type: Number,
-        min: [1, "Please enter valid price"]
+        min: [1, "Please enter valid price"],
     },
 
     discount: {
         type: Number,
         default: 0,
-    }
-})
+    },
+
+    category: {
+        type: String,
+        enum: ["fiction", "non-fiction"],
+    },
+
+    genere: [String],
+});
 
 const Book = mongoose.model("Book", bookSchama);
+
+Book.insertMany([
+    // {title: "Physics XII", author: "HC Verma", description: "A physics World", price: 850, discount: 150, category: "non-fiction"},
+    // {title: "Mathematics X", author: "RD Sharma", description: "A mathematical World", price: 1250, discount: 250, category: "non-fiction"},
+    // {title: "Wings of fire", author: "APJ Abdul Kalam", description: "Auto biography of APJ Abdul Kalam", price: 350, category: "non-fiction"},
+
+    // {title: "Wings of fire", author: "APJ Abdul Kalam", description: "Auto biography of APJ Abdul Kalam", price: 350, discount: 150, category: "auto-biography"}, // => Invalid because enum is different from listed
+    // {title: "Wings of fire", author: "APJ Abdul Kalam", description: "Auto biography of APJ Abdul Kalam", price: 0, discount: 150, category: "fiction"}, // => Invalid because price is less then 1
+
+    // {title: "Marvel Comics", author: "Stan Lee", description: "The Marvel chenematic universe", price: 700, discount: 50, category: "fiction", genere: ["comics", "superheros", "fiction"]},
+
+])
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+
+Book.find({}).then((data) => console.log(data));
