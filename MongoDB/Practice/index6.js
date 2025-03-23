0c2a4096b08557cd4608b1a3f755a31157e1e1a4
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-03-23 07:08:21 +0530

    Improvement 80

--- MongoDB/Practice/index6.js
+++ MongoDB/Practice/index6.js
@@ -3,13 +3,14 @@ const app = express();
 
 const mongoose = require("mongoose");
 
+app.get("/", (req, res) => {
+    res.send("Working");
+})
+
 app.listen(8080, () => {
     console.log("App was listen on port: 8080");
 });
 
-app.get((res, req) => {
-    res.send("Working");
-})
 
 main()
     .then(() => console.log("Connection Successful..."))
