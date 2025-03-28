5d622aafd8a49728a657ccda4f40d756833b0730
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-03-28 06:19:59 +0530

    Improvement 81

---
+++ MongoDB/05.Mongo-with-Express/backend-mongo/init.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const chat = require("./models/chat.js");
+
+main()
+    .then(() => console.log("Connection Successful..."))
+    .catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+let allChats = [
+    {from: "Sneha Chaudhary", to: "Chandan Chaudhary", msg: "How are you?", created_at: new Date()},
+    {from: "Anshika Verma", to: "Anand Sukla", msg: "Please call me", created_at: new Date()},
+    {from: "Safali Singh", to: "Nilam Mishra", msg: "Who are you?", created_at: new Date()},
+    {from: "Jagat Patel", to: "Satish Yadav", msg: "Where are you?", created_at: new Date()},
+    {from: "Akash Pathak", to: "Divaynsh Singh", msg: "When did you come?", created_at: new Date()},
+    {from: "Manish Tripathi", to: "Harshit Debey", msg: "Meet me tommarow", created_at: new Date()},
+]
