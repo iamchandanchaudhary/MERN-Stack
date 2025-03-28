5d9f115eaad2b456ca45050bb48a22fef39f5594
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-03-28 07:24:37 +0530

    Upload 18 (MongoDB)

--- MongoDB/05.Mongo-with-Express/backend-mongo/index.js
+++ MongoDB/05.Mongo-with-Express/backend-mongo/index.js
@@ -4,18 +4,9 @@ const path = require("path");
 
 const mongoose = require("mongoose");
 const Chat = require("./models/chat"); // ==> Chat model
-const { time } = require("console");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
-app.get("/", (req, res) => {
-    res.send("Working properly.");
-})
-
-app.listen(8080, () => {
-    console.log("Application was listning on port 8080");
-})
 
 main()
     .then(() => console.log("Connection Successful"))
@@ -25,8 +16,27 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
 
+
+app.listen(8080, () => {
+    console.log("Application was listning on port 8080");
+})
+
+app.get("/", (req, res) => {
+    res.send("Working properly.");
+})
+
 // Chat.insertMany([
 //     {from: "Sneha Chaudhary", to: "Neha Chaudhary", msg: "How are you?", created_at: new Date()},
 //     {from: "Chandan Chaudhary", to: "Sheshmani Chaudhary", msg: "Call me", created_at: new Date()},
 // ])
 //     .then((data) => console.log(data));
+
+// Index routes 
+app.get("/chats", async(req, res) => {
+    let chats = await Chat.find();
+    console.log(chats);
+    // res.send("Working");
+    res.render("index.ejs", { chats });
+})
+
+// Chat.find({}).then((data) => console.log(data)).catch((err) => console.log(err));
