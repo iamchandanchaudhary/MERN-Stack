7f607386556bbabfa797f2e60290ba1c4de9094d
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-03-31 10:34:09 +0530

    Upload 20 (MongoDB)

--- MongoDB/05.Mongo-with-Express/backend-mongo/index.js
+++ MongoDB/05.Mongo-with-Express/backend-mongo/index.js
@@ -68,3 +68,11 @@ app.post("/chats", (req, res) => {
 
     res.redirect("/chats");
 })
+
+// edit route
+app.get("/chats/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    let chat = await Chat.findById(id);
+
+    res.render("editChat.ejs", { chat });
+})
