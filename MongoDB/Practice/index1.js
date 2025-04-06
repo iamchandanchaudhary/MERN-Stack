80d5c36f42cd02e4e49ff78b37e09e235b016f2a
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-04-06 16:30:25 +0530

    Improvement 88

--- MongoDB/Practice/index1.js
+++ MongoDB/Practice/index1.js
@@ -2,13 +2,20 @@ const mongoose = require("mongoose");
 
 // mongoose.connect("mongodb://127.0.0.1:27017/college");
 
-main()
-    .then(() => console.log("Rquest Received"))
-    .catch((err) => console.log(err));
-
-async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/Practice');
+const connectionDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect('mongodb://127.0.0.1:27017/Practice');
+        console.log(`Rquest Received: ${connectionInstance}`);
+    }
+    catch(err) {
+        console.log("MongoDB connection Error:", err);
+    }
 }
+connectionDB();
+
+// main()
+//     .then(() => console.log(`Rquest Received ${connectionInstance}`))
+//     .catch((err) => console.log(err));
 
 const studentSchema = new mongoose.Schema({
     name: String,
@@ -19,21 +26,21 @@ const studentSchema = new mongoose.Schema({
 
 const Student = mongoose.model("Student", studentSchema);
 
-const stu1 = new Student({
-    name: "Chandan Chaudhary",
-    age: 18,
-    city: "Basti",
-    marks: 98.5
-})
+// const stu1 = new Student({
+//     name: "Chandan Chaudhary",
+//     age: 18,
+//     city: "Basti",
+//     marks: 98.5
+// })
 
-stu1.save()
-    .then((data) => console.log(data));
-    // .catch(() => console.log(err));
+// stu1.save()
+//     .then((data) => console.log(data));
+//     // .catch(() => console.log(err));
 
-Student.insertMany([
-    {name: "Nukhil Verma", age: 21, city: "Lucknow", marks: 91},
-    {name: "Ashok Singh", age: 31, city: "Barabanki", marks: 82},
-    {name: "Manoj Rajput", age: 25, city: "Ayodhya", marks: 92},
-    {name: "Akansha Mishra", age: 20, city: "Gonda", marks: 77}
-])
-    .then((data) => console.log(data))
+// Student.insertMany([
+//     {name: "Nukhil Verma", age: 21, city: "Lucknow", marks: 91},
+//     {name: "Ashok Singh", age: 31, city: "Barabanki", marks: 82},
+//     {name: "Manoj Rajput", age: 25, city: "Ayodhya", marks: 92},
+//     {name: "Akansha Mishra", age: 20, city: "Gonda", marks: 77}
+// ])
+//     .then((data) => console.log(data))
