34ce163bba3601311ec3d5f4fb4697cf283c6a74
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-04-21 14:29:59 +0530

    Image Improvement

--- React.js/08.React-Router-DOM/route-project/src/components/Contacts.jsx
+++ React.js/08.React-Router-DOM/route-project/src/components/Contacts.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
 
 function Contacts() {
     return (
@@ -8,6 +9,13 @@ function Contacts() {
 
                 <div className="text-[#111] m-5">
                     <h1 className="text-4xl font-bold">This is a Contact Section.</h1>
+
+                    {/* Link tag is use to redirect on another route */}
+                    <Link to={"/"}>
+                    {/* <a href="/"> */}
+                        <button className="bg-black px-3 py-1 text-[#fff] rounded-md mt-5" type="button">Back to Home</button>
+                    {/* </a> */}
+                    </Link>
                 </div>
             </section>
         </>
