ac89b86c068679ac594b5b802b656fe823155f8a
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-06-22 14:31:21 +0530

    router-dom

--- React.js/08.React-Router-DOM/BrowserRouter/src/components/Navbar.jsx
+++ React.js/08.React-Router-DOM/BrowserRouter/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <section className="flex justify-center items-center w-full h-16 px-10 bg-blue-500 text-[#fff]">
 
-            <div className="flex justify-center items-center gap-5">
+            <div className="flex justify-center items-center gap-5 menu">
                 <NavLink to={"/"} className="menu hover:bg-[#fff] hover:text-[#000] px-3 py-1 rounded-md hover:drop-shadow-md transition-all duration-300 ease-in-out">Home</NavLink>
                 <NavLink to={"/contacts"} className="menu hover:bg-[#fff] hover:text-[#000] px-3 py-1 rounded-md hover:drop-shadow-md transition-all duration-300 ease-in-out">Contacts</NavLink>
                 <NavLink to={"/services"} className="menu hover:bg-[#fff] hover:text-[#000] px-3 py-1 rounded-md hover:drop-shadow-md transition-all duration-300 ease-in-out">Services</NavLink>
--- React.js/08.React-Router-DOM/createBrowserRouter/src/components/Navbar.jsx
+++ React.js/08.React-Router-DOM/createBrowserRouter/src/components/Navbar.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Navbar() {
+
+  // let isActive = ({isActive}) => isActive ? "active-link" : "";
+
   return (
     <section className="flex justify-between items-center w-full h-16 px-10 bg-blue-500 text-[#fff]">
 
         <div className="flex justify-center items-center gap-5">
-            <NavLink to={"/"} className="menu hover:bg-[#fff] hover:text-[#000] px-3 py-1 rounded-md hover:drop-shadow-md transition-all duration-300 ease-in-out">Home</NavLink>
-            <NavLink to={"/dashboard"} className="menu hover:bg-[#fff] hover:text-[#000] px-3 py-1 rounded-md hover:drop-shadow-md transition-all duration-300 ease-in-out">Dashboard</NavLink>
-            <NavLink to={"/community"} className="menu hover:bg-[#fff] hover:text-[#000] px-3 py-1 rounded-md hover:drop-shadow-md transition-all duration-300 ease-in-out">Community</NavLink>
+            <NavLink to={"/"} className={({isActive}) => isActive ? "active-link" : ""}>Home</NavLink>
+            <NavLink to={"/dashboard"} className={({isActive}) => isActive ? "active-link" : ""}>Dashboard</NavLink>
+            <NavLink to={"/community"} className={({isActive}) => isActive ? "active-link" : ""}>Community</NavLink>
         </div>
     </section>
   )
