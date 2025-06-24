b9b210e7ab26580c2dd4db89f2d50ddc5823ab77
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-06-24 11:23:56 +0530

    React Form Hook

--- React.js/09.React-Form/form-project/src/components/ReactForm.jsx
+++ React.js/09.React-Form/form-project/src/components/ReactForm.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function ReactForm() {
 
-    let [fullName, setFullName] = useState("Chandan");
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm();
 
-    let handleName = (event) => {
+    let [firstName, setFullName] = useState("Chandan");
+    let [lastName, setFirstName] = useState("Chaudhary");
+    let [password, setPassword] = useState("");
+
+    let handleValue = (event) => {
         // console.log(event.target.value);
         setFullName(event.target.value);
     }
@@ -12,7 +22,24 @@ function ReactForm() {
     return (
         <div>
             <form action="">
-                <input type="text" placeholder="Enter name" value={fullName} onChange={handleName} />
+                <div>
+                    <label>First Name: </label>
+                    <input type="text" placeholder="Enter firstName" value={firstName} onChange={handleValue} {...register("example")} />
+                </div>
+                <br />
+
+                <div>
+                    <label>Last Name: </label>
+                    <input type="text" placeholder="Enter lastName" value={lastName} onChange={handleValue} {...register("example")} />
+                </div>
+                <br />
+
+                <div>
+                    <label>Password: </label>
+                    <input type="password" placeholder="Enter password" value={password} onChange={handleValue} {...register("example")} />
+                </div>
+                <br />
+
                 <button type="submit">Submit</button>
             </form>
         </div>
