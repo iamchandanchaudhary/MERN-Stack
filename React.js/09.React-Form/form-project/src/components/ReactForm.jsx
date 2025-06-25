6dde06c5d8106a9a8c60f23ea17e0b604a5c39c0
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-06-25 12:02:37 +0530

    Improvement 103

--- React.js/09.React-Form/form-project/src/components/ReactForm.jsx
+++ React.js/09.React-Form/form-project/src/components/ReactForm.jsx
@@ -10,9 +10,9 @@ function ReactForm() {
         formState: { errors },
     } = useForm();
 
-    let [firstName, setFullName] = useState("Chandan");
-    let [lastName, setFirstName] = useState("Chaudhary");
-    let [password, setPassword] = useState("");
+    // let [firstName, setFullName] = useState("Chandan");
+    // let [lastName, setFirstName] = useState("Chaudhary");
+    // let [password, setPassword] = useState("");
 
     // let handleValue = (event) => {
     //     console.log(event.target.value);
@@ -26,19 +26,22 @@ function ReactForm() {
             <form action="" onSubmit={handleSubmit(onSubmit)}>
                 <div>
                     <label>First Name: </label>
-                    <input type="text" placeholder="Enter firstName" {...register("firstName")} />
+                    <input type="text" placeholder="Enter firstName" {...register("firstName", { required: true, minLength: 4, maxLength: 20 })} />
+                    {errors.firstName && <p style={{color: "red"}}>First name is required.</p>}
                 </div>
                 <br />
 
                 <div>
                     <label>Last Name: </label>
-                    <input type="text" placeholder="Enter lastName" {...register("lastName")} />
+                    <input type="text" placeholder="Enter lastName" {...register("lastName", { required: true, minLength: 4, maxLength: 20 })} />
+                    {errors.lastName && <p style={{color: "red"}}>{errors.lastName.message}</p>}
                 </div>
                 <br />
 
                 <div>
                     <label>Password: </label>
-                    <input type="password" placeholder="Enter password" {...register("password")} />
+                    <input type="password" placeholder="Enter password" {...register("password", { required: {value: true, message: "required field."}, minLength: {value: 4, message: "min 4 length is possible."}, maxLength: {value: 12, message: "max 12 length is possible."} })} />
+                    {errors.password && <p style={{color: "red"}}>{errors.password.message}</p>}
                 </div>
                 <br />
 
