2b698b78eca2d64cfe84da22405034d76a86f694
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-09-11 20:29:17 +0530

    Improvement 112

--- MongoDB/Practice/College/Practice 01.js
+++ MongoDB/Practice/College/Practice 01.js
@@ -1,4 +1,7 @@
 
+    // ==> CRUD Operations using MongoDB
+
+    // ==> Create
     // Insert one document
     db.students.insertOne({
         name: "Chandan",
@@ -12,15 +15,34 @@
         { name: "Ayush", age: 21, course: "B.Tech" }
     ]);
 
+    // ==> Read
     // Find all documents
     db.students.find();
 
     // Find one document
     db.students.findOne({ name: "Chandan" });
 
-    // Find with condition (age > 21)
-    db.students.find({ age: { $gt: 21 } });
+    // Find with condition (age > 20)
+    db.students.find({ age: { $gt: 20 } });
 
     // Find specific fields (only name, no _id)
     db.students.find({}, { name: 1, _id: 0 });
-    
+
+    // ==> Update
+    // Update one document
+    db.students.updateOne(
+        { name: "Chandan" },         // filter
+        { $set: { age: 20 } }      // update
+    );
+
+    // Update multiple documents
+    db.students.updateMany(
+        { course: "BCA" },
+        { $set: { course: "BCA - Honors" } }
+    );
+
+    // Replace a whole document
+    db.students.replaceOne(
+        { name: "Deepanshu" },
+        { name: "Deepanshu", age: 22, course: "M.Tech" }
+    );
