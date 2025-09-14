47cc171ee37a3a0eea7b069652e7daaf784308dc
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-09-14 14:03:21 +0530

    Improvement 117

--- Express.js/Practice/Ulr Building.js
+++ Express.js/Practice/Ulr Building.js
@@ -1,16 +1,15 @@
+// Static Url
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
 
-    // Static Url
-    app.get('/about', (req, res) => {
-        res.send('About Page');
-    });
+// Dynamic Url
+app.get('/products/:id', (req, res) => {
+    res.send(`Product ID: ${req.params.id}`);
+});
 
-    // Dynamic Url
-    app.get('/products/:id', (req, res) => {
-        res.send(`Product ID: ${req.params.id}`);
-    });
-
-    // Query Url
-    app.get('/search', (req, res) => {
-        const { q, category } = req.query;
-        res.send(`Searching for ${q} in ${category}`);
-    });
+// Query Url
+app.get('/search', (req, res) => {
+    const { q, category } = req.query;
+    res.send(`Searching for ${q} in ${category}`);
+});
--- MongoDB/Practice/College/Model.js
+++ MongoDB/Practice/College/Model.js
@@ -1,11 +1,10 @@
+// models/User.js
+const mongoose = require("mongoose");
 
-  // models/User.js
-  const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+});
 
-  const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-  });
-
-  module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
