c68db7c2ed4a4f17790bb255452e7326787ffd77
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-11-20 08:46:23 +0530

    Improvement 124

--- React.js/10.Redux-Toolkit/redux-project/src/redux/store.js
+++ React.js/10.Redux-Toolkit/redux-project/src/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../functions/counter/counterSlice.jsx';
 
-export const store = configureStore({
+export default configureStore({
   reducer: {
-    
+    counter: counterReducer
   }
-})
+});
