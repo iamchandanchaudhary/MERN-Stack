5d268e887b8d15fe9a61a6c46de4b3630223446f
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-11-30 00:11:28 +0530

    MongoDB with Node.js

---
+++ College Work Full-Stack/MongoDB in NodeJS/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/mydatabase", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("✅ MongoDB connected successfully");
+  } catch (error) {
+    console.error("❌ MongoDB connection failed");
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
