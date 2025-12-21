da9ef31453979180fa42ad39707f60a0395cc358
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-12-21 18:51:35 +0530

    UserContextProvider

--- React.js/05.Hooks-and-More/miniContext/src/App.jsx
+++ React.js/05.Hooks-and-More/miniContext/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useState } from 'react';
 import './App.css';
+import UserContextProvider from './context/UserContextProvider';
 
 const App = () => {
 
   return (
-    <>
-
-    </>
+    <UserContextProvider>
+      <h2>Hello 👋, from Chandan Chaudhary</h2>
+    </UserContextProvider>
   )
 }
 
