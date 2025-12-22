ebdcd40bf8ebd9fe8ff54fab56748675c6e389f8
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-12-22 17:44:22 +0530

    Improvement 125

--- React.js/Projects-using-React/themeChanger/src/App.jsx
+++ React.js/Projects-using-React/themeChanger/src/App.jsx
@@ -1,11 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { ThemeProvider } from './context/theme';
 
-function App() {
+const App = () => {
+
+  const [themeMode, setThemeMode] = useState();
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  }
+
+  // changing the theme
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
   return (
-    <>
-    </>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
