dc61ac508922ec20ae288312a26988fb91d83a32
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-12-26 17:44:47 +0530

    adding functionality

--- React.js/Projects-using-React/todoApp-localStorage/src/Components/TodoItem.jsx
+++ React.js/Projects-using-React/todoApp-localStorage/src/Components/TodoItem.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useTodo } from '../Contexts/TodoContext';
 
-const TodoItem = () => {
+const TodoItem = ({ todo }) => {
+
+    const [isTodoEditable, setIsTodoEditable] = useState(false);
+    const [todoMsg, setTodoMsg] = useState(todo.todo);
+    const {editTodo, deleteTodo, toggleComplete} = useTodo();
+
+    const updateTodo = () => {
+        editTodo(todo.id, {...todo, todo: todoMsg})
+        setIsTodoEditable(false)
+    }
+
+    const toggleCompleted = () => {
+        toggleComplete(todo.id);
+    }
+
     return (
         <div className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"}`}>
             <input
